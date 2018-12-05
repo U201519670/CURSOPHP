516d0537dcaa5cdcf2c50a7fb65391a174fb392b
Yesi Days <silvercorp@gmail.com>
2018-12-04 18:42:23 -0600

    Clase 3 + BD

--- poo/clase3/includes/Estudiante.inc.php
+++ poo/clase3/includes/Estudiante.inc.php
@@ -1,6 +1,15 @@
 <?php
 
-	class Estudiante {
+	class Estudiante extends Persona {
+
+		public function __construct($nombre, $apellido, $email) {
+			parent::__construct($nombre, $apellido, $email);
+		}
+
+		#Sobreescribir
+		public function bienvenida() {
+			return "Bienvenido {$this->nombre}, tenemos nuevos cursos para ti";
+		}
 
 
 	}
--- poo/clase3/index.php
+++ poo/clase3/index.php
@@ -7,29 +7,31 @@
 
 	<?php
 
-		define('INC', '/includes/');
-		require_once __DIR__ . INC . 'Curso.inc.php';
-		require_once __DIR__ . INC . 'Estudiante.inc.php';
+		#Incluído mi archivo global, todas mis clases.
+		require_once __DIR__ . '/includes/Global.inc.php';
 
-		$curso1 = new Curso('POO en PHP', 'Yesi Days', '3 sesiones', 10, true);
+		#Instanciar un estudiante
+		$estudiante = new Estudiante('Yesi', 'Days', 'silvercorp@gmail.com');
 
-		$curso2 = new Curso('Javascript desde 0', 'Álvaro Felipe', '6 sesiones', 0, true);
+		#var_dump($estudiante);
+		echo "<p>".$estudiante->bienvenida()."</p>";
+		echo "<p>".$estudiante->despedida()."</p>";
+		echo "<p>".$estudiante->validarCompra()."</p>";
 
-		echo $curso1->obtenerTitulo()."<br />";
-		echo $curso1->obtenerProfesor()."<br />";
-
-		$curso1->asignarTitulo('POO en PHP I');
-		echo $curso1->obtenerTitulo()."<br />";
-
-		$curso1->asignarRequerimiento(['PHP desde cero', 'Conocimientos básicos de PHP', 'Ganas de aprender', 'Práctica']);
-
-		echo "<h3>Mis requerimientos para la clase</h3>";
-
-		$curso1->obtenerRequerimiento();
 		echo "<br />";
+		echo $estudiante::MONEDA;
 
-		echo Curso::$bienvenida."<br />";
-		echo Curso::obtenerDenominacion();
+
+
+		#Instanciar un profesor
+		$profesor = new Profesor('Jon', 'Mircha', 'jormircha@gmail.com');
+		#var_dump($profesor);
+		echo "<p>".$profesor->bienvenida()."</p>";
+		echo "<p>".$profesor->despedida()."</p>";
+
+		echo "<br />";
+		echo $profesor::MONEDA;
+
 
 
 
